c2140f7805502dc9a861178c793f89108a06a20c
EmilNielsen <43439030+EmilNielsen@users.noreply.github.com>
2019-01-17 14:26:00 +0100

    Added preivew of last color
    last color preview /w color code
    color codes printed in browser

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-//Variabel til at tage fat i knap
-//-Knap fjernet, variabel kommenteret-
-//var button = document.querySelector("button");
+document.addEventListener('DOMContentLoaded', function () {
+    //Variabel til at tage fat i knap
+    //-Knap fjernet, variabel kommenteret-
+    //var button = document.querySelector("button");
+    var tal = document.querySelector(".tal");
+    var preview = document.querySelector(".preview");
+    var previewTal = document.querySelector(".previewTal");
 
-function RNG(max) {
-    return Math.floor(Math.random() * Math.floor(max) + 1);
-}
+    function RNG(max) {
+        return Math.floor(Math.random() * Math.floor(max) + 1);
+    }
 
-function bgChange() {
-    //Opretter variabler med tilfældig farvekode
-    var R = RNG(255);
-    var G = RNG(255);
-    var B = RNG(255);
-    //sætter body bgfarve til tilfældig kombination af farvekoderne
-    document.body.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
-    //console.log(document.body.style.backgroundColor);
-};
+    function bgChange() {
+        preview.style.backgroundColor = document.body.style.backgroundColor;
+        previewTal.innerHTML = document.body.style.backgroundColor;
+        //Opretter variabler med tilfældig farvekode
+        var R = RNG(255);
+        var G = RNG(255);
+        var B = RNG(255);
+        //sætter body bgfarve til tilfældig kombination af farvekoderne
+        document.body.style.backgroundColor = "rgb(" + R + ", " + G + ", " + B + ")";
+        //console.log(document.body.style.backgroundColor);
+        tal.innerHTML = "R: " + R + "<br>" + "G: " + G + "<br>" + "B: " + B;
+    };
 
-//Gentager bgChange function hvert sekund
-setInterval(bgChange, 1000);
+    //Gentager bgChange function hvert sekund
+    setInterval(bgChange, 1000);
+});
